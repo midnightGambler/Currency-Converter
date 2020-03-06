ca8e67576c0910ce5598c5849920e33e2960bfa3
midnightGambler <42617615+midnightGambler@users.noreply.github.com>
2020-03-07 00:45:38 +0300

    REWRITING installed styled-components+basic styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import { colors } from "./styles/variables";
+import Container from "./components/UI/Container";
 
-const App = () => <h1>Hello world</h1>;
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Josefin Sans', sans-serif;
+    background-color: ${colors.bgDark};
+  }
+`;
+
+const StyledH1 = styled.h1`
+  color: ${colors.textPrimary};
+  font-size: 3rem;
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Container>
+      <StyledH1>Hello world</StyledH1>
+    </Container>
+  </>
+);
 
 ReactDom.render(<App />, document.getElementById("app"));
---
+++ src/styles/variables.js
@@ -0,0 +1,6 @@
+export const colors = {
+  textPrimary: "#ff6464",
+  textSecondary: "#8b4367",
+  bgLight: "#543864",
+  bgDark: "#01024e"
+};
